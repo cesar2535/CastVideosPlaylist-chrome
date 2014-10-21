7e4b570406ea2a01ff249fb876a85eae9a00a966
Cesar <cesar6134@gmail.com>
2014-10-21 12:38:30 +0800

    Modified chrome cast app id

--- playlist_sender.js
+++ playlist_sender.js
@@ -66,7 +66,7 @@ if (!chrome.cast || !chrome.cast.isAvailable) {
  * initialization
  */
 function initializeCastApi() {
-  var applicationID = '3F4C1CF5';
+  var applicationID = '8A89B29F';
   var sessionRequest = new chrome.cast.SessionRequest(applicationID);
   var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
     sessionListener,
